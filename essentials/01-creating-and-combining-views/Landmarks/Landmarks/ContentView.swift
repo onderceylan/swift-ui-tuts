fcc2ca85896ebb3533d1a7298235700f0eb941b8
Önder Ceylan <onderceylan@gmail.com>
2020-04-12 16:02:38 +0200

    Added Landmarks project based on creating and combining views tutorial

---
+++ essentials/01-creating-and-combining-views/Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Önder Ceylan on 12.04.2020.
+//  Copyright © 2020 Önder Ceylan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 300)
+            
+            CircleImage().offset(y: -130).padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+            }.padding()
+            
+            Spacer()
+        }
+            
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
